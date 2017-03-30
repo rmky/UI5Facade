e48013a6b3fc25101df87501556ba1a67acd1bb5
Andrej Kabachnik <andrej_ka@gmx.de>
2017-03-31 00:15:50 +0200

    FIX DataTable

--- Template/Elements/ui5DataTable.php
+++ Template/Elements/ui5DataTable.php
@@ -2,6 +2,7 @@
 namespace exface\OpenUI5Template\Template\Elements;
 
 use exface\Core\Widgets\DataTable;
+use exface\Core\Widgets\DataColumn;
 
 /**
  * 
@@ -16,76 +17,23 @@ class ui5BasicElement extends ui5AbstractElement {
 	}
 	
 	function generate_js(){
+		$widget = $this->get_widget();
+		
+		// Columns
+		foreach ($widget->get_columns() as $column){
+			$columns_js .= $this->build_js_column_def($column);
+		}
+		
 		$js = <<<JS
 		
-	var vData=[
-           {assID:"EM123456", name:"Bharath S", linkText:"Cognizant Technology Solutions", href:"http://www.cognizant.com", gender:"Male", mobile:"9934307162", rating:5},
-           {assID:"EM263521", name:"Arun M", linkText:"Cognizant Technology Solutions", href:"http://www.cognizant.com", gender:"Male", mobile:"9786721460", rating:3},
-           {assID:"EM323455", name:"Anitha", linkText:"Cognizant Technology Solutions", href:"http://www.cognizant.com", gender:"Female", mobile:"9524396759", rating:4},
-           {assID:"EM237652", name:"Ganesh", linkText:"Cognizant Technology Solutions", href:"http://www.cognizant.com", gender:"Male", mobile:"9876543210", rating:1},
-           {assID:"EM398454", name:"Ajai", linkText:"Cognizant Technology Solutions", href:"http://www.cognizant.com", gender:"Male", mobile:"9576113218", rating:4},
-           {assID:"EM348092", name:"Pranav", linkText:"Cognizant Technology Solutions", href:"http://www.cognizant.com", gender:"Male", mobile:"9576113218", rating:5}
-          ];
-		
 	var oTable = new sap.ui.table.Table({
-    visibleRowCount: 20,                                           // How much rows you want to display in the table
+    visibleRowCount: {$widget->get_paginate_default_page_size()},
     selectionMode: sap.ui.table.SelectionMode.Single, //Use Singe or Multi
     navigationMode: sap.ui.table.NavigationMode.Paginator, //Paginator or Scrollbar
-    fixedColumnCount: 3,                     // Freezes the number of columns
     enableColumnReordering:true,       // Allows you to drag and drop the column and reorder the position of the column
-    width:"1024px"                              // width of the table
   });
-// Use the Object defined for table to add new column into the table
-    oTable.addColumn(new sap.ui.table.Column({
 
-    label: new sap.ui.commons.Label({text: "Associate ID"}),             // Creates an Header with value defined for the text attribute
-    template: new sap.ui.commons.TextField().bindProperty("value", "assID"), // binds the value into the text field defined using JSON
-    sortProperty: "assID",        // enables sorting on the column
-    filterProperty: "assID",       // enables set filter on the column
-    width: "125px"                  // width of the column
-}));
-    oTable.addColumn(new sap.ui.table.Column({
-
-    label: new sap.ui.commons.Label({text: "Associate Name"}),
-    template: new sap.ui.commons.TextField().bindProperty("value", "name"),
-    sortProperty: "name",
-    filterProperty: "name",
-    width: "125px"
-}));
-    oTable.addColumn(new sap.ui.table.Column({
-    label: new sap.ui.commons.Label({text: "Company"}),
-    template: new sap.ui.commons.Link().bindProperty("text", "linkText").bindProperty("href", "href"),
-    sortProperty: "linkText",
-    filterProperty: "linkText",
-    width: "200px"
-}));
-     oTable.addColumn(new sap.ui.table.Column({
-
-     label: new sap.ui.commons.Label({text: "Gender"}),
-     template: new sap.ui.commons.ComboBox(
-                    {items: [new sap.ui.core.ListItem({text: "Female"}),
-                    new sap.ui.core.ListItem({text: "Male"})]}).bindProperty("value","gender"),
-     sortProperty: "gender",
-     filterProperty: "gender",
-     width: "75px"
-    }));
-    oTable.addColumn(new sap.ui.table.Column({
-    label: new sap.ui.commons.Label({text: "Contact Number"}),
-    template: new sap.ui.commons.TextField().bindProperty("value", "mobile"),
-    sortProperty: "mobile",
-    filterProperty: "mobile",
-    width: "75px"
-}));
-    oTable.addColumn(new sap.ui.table.Column({
-
-    label: new sap.ui.commons.Label({text: "Rating"}),
-    template: new sap.ui.commons.RatingIndicator().bindProperty("value", "rating"),
-    sortProperty: "rating",
-    filterProperty: "rating",
-    width: "100px"
-}));
-
-
+	{$columns_js}
 
 	// Now, create a model to bind the table rows.
      //Create a model and bind the table rows to this model
@@ -95,7 +43,7 @@ class ui5BasicElement extends ui5AbstractElement {
 
      oTable.setModel(oModel);                                                                                  
 
-     oTable.bindRows("/modelData");                              // binding all the rows into the model
+     oTable.bindRows("/data");                              // binding all the rows into the model
 
      //Initially sort the table
 
@@ -108,8 +56,25 @@ JS;
 	protected function build_js_data_source($js_filters = ''){
 		$url = $this->get_ajax_url() . '&action=' . $this->get_widget()->get_lazy_loading_action() . '&resource=' . $this->get_page_id() . '&element=' . $this->get_widget()->get_id() . '&object=' . $this->get_widget()->get_meta_object()->get_id();
 		
-		return <<<JS
+		/*return <<<JS
 		oModel.setData({modelData: vData});
+JS;*/
+		return <<<JS
+		oModel.loadData("{$url}");
+JS;
+	}
+		
+	protected function build_js_column_def(DataColumn $column){
+		return <<<JS
+
+ oTable.addColumn(new sap.ui.table.Column({
+
+    label: new sap.ui.commons.Label({text: "{$column->get_caption()}"}),
+    template: new sap.ui.commons.TextField().bindProperty("value", "{$column->get_data_column_name()}"),
+    sortProperty: "{$column->get_data_column_name()}",
+    filterProperty: "{$column->get_data_column_name()}"
+}));
+
 JS;
 	}
 }

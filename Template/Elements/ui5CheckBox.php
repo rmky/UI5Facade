3dc26657dfbe7fb84eb4fd8184c48eceb69b1448
Andrej Kabachnik <andrej_ka@gmx.de>
2017-12-11 12:42:28 +0100

    NEW CheckBox widget

---
+++ Template/Elements/ui5CheckBox.php
@@ -0,0 +1,33 @@
+<?php
+namespace exface\OpenUI5Template\Template\Elements;
+
+/**
+ * Generates OpenUI5 inputs
+ *
+ * @author Andrej Kabachnik
+ *        
+ */
+class ui5CheckBox extends ui5Input
+{
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\OpenUI5Template\Template\Elements\ui5Text::buildJsElementConstructor()
+     */
+    protected function buildJsElementConstructor()
+    {
+        return <<<JS
+        new sap.m.CheckBox("{$this->getId()}", {
+            {$this->buildJsProperties()}
+        })
+JS;
+    }
+    
+    protected function buildJsPropertyValue()
+    {
+        $value = $this->getWidget()->getValueWithDefaults();
+        return ($value ? ', selected: true' : '');
+    }
+}
+?>

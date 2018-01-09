1541f6ed3afe31d7e9d570eb897720e20734e3df
Andrej Kabachnik <andrej_ka@gmx.de>
2018-01-09 10:44:08 +0100

    NEW element for InputDate

---
+++ Template/Elements/ui5InputDate.php
@@ -0,0 +1,30 @@
+<?php
+namespace exface\OpenUI5Template\Template\Elements;
+
+use exface\Core\Widgets\InputDate;
+
+/**
+ * 
+ * @method InputDate getWidget()
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
+class ui5InputDate extends ui5Input
+{
+    
+    public function buildJsElementConstructor()
+    {
+        return <<<JS
+
+        new sap.m.DatePicker("{$this->getId()}", {
+            {$this->buildJsProperties()},
+			valueFormat: "yyyy-MM-dd",
+            displayFormat: "short"
+		})
+
+JS;
+    }
+    
+}
+?>

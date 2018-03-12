a263dde8e17d8877c07872867a1ae1bed1f441f1
Andrej Kabachnik <andrej_ka@gmx.de>
2018-03-12 10:30:54 +0100

    REFACTOR lazy loading defaults

--- Template/Elements/ui5DataTable.php
+++ Template/Elements/ui5DataTable.php
@@ -264,11 +264,7 @@ JS;
      */
     protected function isLazyLoading()
     {
-        $widget_option = $this->getWidget()->getLazyLoading();
-        if (is_null($widget_option)) {
-            return true;
-        }
-        return $widget_option;
+        return $this->getWidget()->getLazyLoading(false);
     }
 
     /**

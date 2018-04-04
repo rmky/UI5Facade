51aa9f3a1e83697f4f8890cbd1437a777675a592
Andrej Kabachnik <andrej_ka@gmx.de>
2018-04-04 17:38:31 +0200

    NEW export of basic webapp structure

---
+++ Actions/ExportFioriWebapp.php
@@ -0,0 +1,166 @@
+<?php
+namespace exface\OpenUI5Template\Actions;
+
+use exface\Core\Actions\DownloadZippedFolder;
+use exface\Core\Interfaces\Tasks\TaskInterface;
+use exface\Core\CommonLogic\ArchiveManager;
+use exface\OpenUI5Template\OpenUI5TemplateApp;
+use exface\Core\Interfaces\Model\UiPageInterface;
+use exface\Core\Factories\UiPageFactory;
+use exface\Core\CommonLogic\Filemanager;
+use exface\Core\Interfaces\AppInterface;
+use exface\Core\DataTypes\StringDataType;
+
+/**
+ * 
+ * @author Andrej Kabachnik
+ * 
+ * @method OpenUI5TemplateApp getApp()
+ *
+ */
+class ExportFioriWebapp extends DownloadZippedFolder
+{
+    protected function init()
+    {
+        parent::init();
+        $this->setInputRowsMin(1);
+        $this->setInputRowsMax(1);
+    }
+    
+    protected function createZip(TaskInterface $task) : ArchiveManager
+    {
+        $zip = new ArchiveManager($this->getWorkbench(), $this->getZipPathAbsolute());
+        $input = $this->getInputDataSheet($task);
+        
+        $input->getColumns()->addFromExpression('root_page_alias');
+        $input->getColumns()->addFromExpression('min_ui5_version');
+        $input->getColumns()->addFromExpression('app_id');
+        $input->getColumns()->addFromExpression('app_title');
+        $input->getColumns()->addFromExpression('app_subTitle');
+        $input->getColumns()->addFromExpression('app_shortTitle');
+        $input->getColumns()->addFromExpression('app_info');
+        $input->getColumns()->addFromExpression('app_description');
+        
+        if (! $input->isFresh()) {
+            $input->addFilterFromColumnValues($input->getUidColumn());
+            $input->dataRead();
+        }
+        
+        $row = $input->getRows()[0];
+        $rootPage = UiPageFactory::createFromCmsPage($this->getWorkbench()->getCMS(), $row['root_page_alias']);
+        
+        $webappFolder = $this->exportWebapp($rootPage, $row);
+        $zip->addFolder($webappFolder);
+        
+        $zip->close();
+        return $zip;
+    }
+    
+    protected function exportWebapp(UiPageInterface $rootPage, array $appDataRow) : string
+    {
+        
+        $path = $this->getApp()->getExportFolderAbsolutePath() . DIRECTORY_SEPARATOR . $rootPage->getAliasWithNamespace();
+        
+        if (! file_exists($path)) {
+            Filemanager::pathConstruct($path);
+        } else {
+            $this->getWorkbench()->filemanager()->emptyDir($path);
+        }
+        
+        $path = $path . DIRECTORY_SEPARATOR;
+        
+        $this->exportManifest($appDataRow, $path);
+        $this->exportIndexHtml($appDataRow, $path);
+        $this->exportComponent($appDataRow['app_id'], $path);
+        $this->exportTranslations($rootPage->getApp(), $path);
+        
+        return $path;
+    }
+    
+    protected function exportManifest(array $appDataRow, string $exportFolder) : string
+    {
+        $tpl = file_get_contents($this->getWebappTemplateFolder() . 'manifest.json');
+        $tpl = str_replace('[#app_id#]', $appDataRow['app_id'], $tpl);
+        $json = json_decode($tpl, true);
+        $json['_version'] = $this->getManifestVersion($appDataRow['min_ui5_version']);
+        $json['sap.app']['id'] = $appDataRow['app_id'];
+        $json['sap.app']['title'] = $appDataRow['app_title'];
+        $json['sap.app']['subTitle'] = $appDataRow['app_subTitle'] ? $appDataRow['app_subTitle'] : '';
+        $json['sap.app']['shortTitle'] = $appDataRow['app_shortTitle'] ? $appDataRow['app_shortTitle'] : '';
+        $json['sap.app']['info'] = $appDataRow['app_info'] ? $appDataRow['app_info'] : '';
+        $json['sap.app']['description'] = $appDataRow['app_description'] ? $appDataRow['app_description'] : '';
+        $json['sap.app']['applicationVersion']['version'] = $appDataRow['current_version'];
+        $json['sap.ui5']['dependencies']['minUI5Version'] = $appDataRow['min_ui5_version'];
+        
+        $target = $exportFolder . 'manifest.json';
+        file_put_contents($target, json_encode($json, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES));
+        return $target;
+    }
+    
+    protected function exportComponent(string $webapp_id, string $exportFolder) : string
+    {
+        $tpl = file_get_contents($this->getWebappTemplateFolder() . 'Component.js');
+        $target = $exportFolder . 'Component.js';
+        file_put_contents($target, str_replace('[#app_id#]', $webapp_id, $tpl));
+        return $target;
+    }
+    
+    protected function exportTranslations(AppInterface $app, string $exportFolder) : ExportFioriWebapp
+    {
+        $folder = $app->getDirectoryAbsolutePath() . DIRECTORY_SEPARATOR . 'Translations' . DIRECTORY_SEPARATOR;
+        $defaultLang = $app->getDefaultLanguageCode();
+        foreach (glob($folder . "*.json") as $filename) {
+            $this->exportTranslation($filename, $defaultLang, $exportFolder);
+        }
+        return $this;
+    }
+    
+    protected function exportTranslation(string $filePath, string $defaultLang, string $exportFolder) : string
+    {
+        $json = json_decode(file_get_contents($filePath), true);
+        $filename = pathinfo($filePath, PATHINFO_FILENAME);
+        $lang = StringDataType::substringAfter($filename, '.', false, false, true);
+        $output = '';
+        foreach ($json as $key => $text) {
+            $output .= $key . '=' . $text. "\n";
+        }
+        $i18nSuffix = (strcasecmp($lang, $defaultLang) === 0) ? '' : '_' . $lang;
+        $i18nFolder = $exportFolder . 'i18n' . DIRECTORY_SEPARATOR;
+        if (! file_exists($i18nFolder)) {
+            Filemanager::pathConstruct($i18nFolder);
+        }
+        $i18nFile = $i18nFolder . 'i18n' . $i18nSuffix . '.properties';
+        file_put_contents($i18nFile, $output);
+        return $i18nFile;
+    }
+    
+    protected function getWebappTemplateFolder() : string
+    {
+        return $this->getApp()->getDirectoryAbsolutePath() . DIRECTORY_SEPARATOR . 'Templates' . DIRECTORY_SEPARATOR . 'js' . DIRECTORY_SEPARATOR . 'webapp' . DIRECTORY_SEPARATOR;
+    }
+    
+    protected function getManifestVersion(string $ui5Version) : string
+    {
+        switch (true) { 
+            case strcmp($ui5Version, '1.30') < 0: return '1.0.0';
+            case strcmp($ui5Version, '1.32') < 0: return '1.1.0';
+            case strcmp($ui5Version, '1.34') < 0: return '1.2.0';
+            case strcmp($ui5Version, '1.38') < 0: return '1.3.0';
+            case strcmp($ui5Version, '1.42') < 0: return '1.4.0';
+            case strcmp($ui5Version, '1.46') < 0: return '1.5.0';
+            case strcmp($ui5Version, '1.48') < 0: return '1.6.0';
+            case strcmp($ui5Version, '1.50') < 0: return '1.7.0';
+            case strcmp($ui5Version, '1.52') < 0: return '1.8.0';
+            default: return '1.9.0';
+        }
+    }
+    
+    protected function exportIndexHtml(array $appDataRow, string $exportFolder) : string
+    {
+        $tpl = file_get_contents($this->getWebappTemplateFolder() . 'index.html');
+        $target = $exportFolder . 'index.html';
+        file_put_contents($target, str_replace('[#app_id#]', $appDataRow['app_id'], $tpl));
+        return $target;
+    }
+    
+}
--- OpenUI5TemplateApp.php
+++ OpenUI5TemplateApp.php
@@ -5,10 +5,12 @@ use exface\Core\Interfaces\InstallerInterface;
 use exface\Core\Templates\AbstractHttpTemplate\HttpTemplateInstaller;
 use exface\Core\CommonLogic\Model\App;
 use exface\Core\Factories\TemplateFactory;
+use exface\Core\CommonLogic\Filemanager;
 
 class OpenUI5TemplateApp extends App
 {
-
+    private $exportPath = null;
+    
     /**
      * {@inheritdoc}
      * 
@@ -23,6 +25,19 @@ class OpenUI5TemplateApp extends App
         $tplInstaller->setTemplate(TemplateFactory::createFromString('exface.OpenUI5Template.OpenUI5Template', $this->getWorkbench()));
         $installer->addInstaller($tplInstaller);
         return $installer;
+    }
+    
+    public function getExportFolderAbsolutePath() : string
+    {
+        if ($this->exportPath === null) {
+            $fm = $this->getWorkbench()->filemanager();
+            $path = $fm::pathJoin([$fm->getPathToBaseFolder(), $this->getConfig()->getOption('WEBAPP_EXPORT.FOLDER_RELATIVE_TO_BASE')]);
+            if (! file_exists($path)) {
+                $fm::pathConstruct($path);
+            }
+            $this->exportPath = $path;
+        }
+        return $this->exportPath;
     }
 }
 ?>

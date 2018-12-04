4020b2861316ae6144f626dd1caf2ab4defa711b
Andrej Kabachnik <andrej_ka@gmx.de>
2018-12-04 18:10:48 +0100

    NEW widget ProgressBar

--- Templates/Elements/ui5ProgressBar.php
+++ Templates/Elements/ui5ProgressBar.php
@@ -13,6 +13,69 @@ use exface\Core\Templates\AbstractAjaxTemplate\Elements\HtmlProgressBarTrait;
  */
 class ui5ProgressBar extends ui5Display
 {
-    use HtmlProgressBarTrait;
+    /**
+     *
+     * {@inheritDoc}
+     * @see \exface\OpenUI5Template\Templates\Elements\ui5AbstractElement::buildJsConstructor()
+     */
+    public function buildJsConstructorForMainControl($oControllerJs = 'oController')
+    {
+        return <<<JS
+        
+        new sap.m.ProgressIndicator("{$this->getid()}", {
+            showValue: true,
+            state: "None",
+    		percentValue: {$this->buildJsValuePercent()},
+            displayValue: {$this->buildJsValue()},
+            {$this->buildJsProperties()}
+    	})
+    	
+JS;
+    }
+            
+    public function buildJsValuePercent() : string
+    {
+       if (! $this->isValueBoundToModel()) {
+            $value = $this->getWidget()->getValueDataType()->parse($this->getWidget()->getValue());
+        } else {
+            $bindingOptions = <<<JS
+                formatter: function(value){
+                    return parseFloat(value);
+                }
+
+JS;
+            $value = $this->buildJsValueBinding($bindingOptions);
+        }
+        return $value;
+    }
+            
+    /**
+     *
+     * {@inheritDoc}
+     * @see \exface\OpenUI5Template\Templates\Elements\ui5Value::buildJsValueBindingPropertyName()
+     */
+    public function buildJsValueBindingPropertyName() : string
+    {
+        return 'percentValue';
+    }
+    
+    /**
+     *
+     * {@inheritDoc}
+     * @see \exface\OpenUI5Template\Templates\Elements\ui5Display::buildJsPropertyAlignment()
+     */
+    protected function buildJsPropertyAlignment()
+    {
+        return '';
+    }
+    
+    /**
+     *
+     * {@inheritDoc}
+     * @see \exface\OpenUI5Template\Templates\Elements\ui5Display::buildJsPropertyWrapping()
+     */
+    protected function buildJsPropertyWrapping()
+    {
+        return '';
+    }
 }
-?>

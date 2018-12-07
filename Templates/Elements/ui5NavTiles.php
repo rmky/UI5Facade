53612d4d1c4517d5db416224e717e60a338139dc
Andrej Kabachnik <andrej_ka@gmx.de>
2018-12-07 16:37:29 +0100

    FIX hide caption of first tile group in NavTiles

--- Templates/Elements/ui5NavTiles.php
+++ Templates/Elements/ui5NavTiles.php
@@ -14,5 +14,13 @@ use exface\Core\Widgets\Container;
  */
 class ui5NavTiles extends ui5Container
 {
-    
+    public function buildJsConstructor($oControllerJs = 'oController') : string
+    {
+        // If the NavTiles is the root widget of a view, it will have a header with the caption
+        // of the first tile group - so just hide the caption of that group to avoid duplicates.
+        if ($this->getWidget()->hasParent() === false) {
+            $this->getWidget()->getWidgetFirst()->setHideCaption(true);
+        }
+        return parent::buildJsConstructor($oControllerJs);
+    }
 }

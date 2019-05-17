ceddd2a328c3c5223b05bddb9455c6e3a20908c4
Andrej Kabachnik <andrej_ka@gmx.de>
2019-05-17 14:22:27 +0200

    NEW icon widget now supports binding to attributes

--- Facades/Elements/UI5Icon.php
+++ Facades/Elements/UI5Icon.php
@@ -18,59 +18,67 @@ class UI5Icon extends UI5Display
      * {@inheritDoc}
      * @see \exface\UI5Facade\Facades\Elements\UI5Display::buildJsConstructor()
      */
-    public function buildJsConstructor($oControllerJs = 'oController') : string
+    public function buildJsConstructorForMainControl($oControllerJs = 'oController') : string
     {
         $icon = $this->buildJsConstructorForIcon();
-        if (! $icon) {
-            return $this->buildJsConstructorForMainControl($oControllerJs);
-        }
         
-        switch ($this->getWidget()->getIconPosition()) {
-            case EXF_ALIGN_RIGHT: return <<<JS
-            
-        new sap.m.HBox({
-            justifyContent: "SpaceAround",
-            alignItems: "Center",
-            items: [
-                {$this->buildJsConstructorForMainControl($oControllerJs)},
-                {$icon}
+        if ($this->getWidget()->hasValueWidget() === true) {
+            $valueElement = $this->getFacade()->getElement($this->getWidget()->getValueWidget());
+            switch ($this->getWidget()->getIconPosition()) {
+                case EXF_ALIGN_RIGHT: $js = <<<JS
                 
-            ]
-        })
-        
-JS;
-            case EXF_ALIGN_CENTER: return <<<JS
+            new sap.m.HBox({
+                justifyContent: "SpaceAround",
+                alignItems: "Center",
+                items: [
+                    {$valueElement->buildJsConstructorForMainControl($oControllerJs)},
+                    {$icon}
+                    
+                ]
+            })
             
-        new sap.m.VBox({
-            width: "100%",
-            alignItems: "Center",
-            items: [
-                {$icon}.addStyleClass("sapUiSmallMargin"),
-                {$this->buildJsConstructorForMainControl($oControllerJs)}
-            ]
-        })
-        
 JS;
-            default: return <<<JS
-
-        new sap.m.HBox({
-            justifyContent: "SpaceAround",
-            alignItems: "Center",
-            items: [
-                {$icon},
-                {$this->buildJsConstructorForMainControl($oControllerJs)}
-            ]
-        })
-
+                case EXF_ALIGN_CENTER: $js = <<<JS
+                
+            new sap.m.VBox({
+                width: "100%",
+                alignItems: "Center",
+                items: [
+                    {$icon}.addStyleClass("sapUiSmallMargin"),
+                    {$valueElement->buildJsConstructorForMainControl($oControllerJs)}
+                ]
+            })
+            
 JS;
+                default: $js = <<<JS
+    
+            new sap.m.HBox({
+                justifyContent: "SpaceAround",
+                alignItems: "Center",
+                items: [
+                    {$icon},
+                    {$valueElement->buildJsConstructorForMainControl($oControllerJs)}
+                ]
+            })
+    
+JS;
+            }
+        } else {
+            $js = $icon;
         }
+        return $js;
     }
     
+    /**
+     * 
+     * @return string
+     */
     public function buildJsConstructorForIcon() : string
     {
         $widget = $this->getWidget();
-        $icon = $widget->getIcon();
-        if (! $icon) {
+        
+        $iconSrc = $this->buildJsPropertyValue();
+        if ($iconSrc === '') {
             return '';
         }
         
@@ -84,10 +92,59 @@ JS;
         return <<<JS
 
             new sap.ui.core.Icon({
-                src: "{$this->getIconSrc($icon)}",
+                {$iconSrc}
                 {$size}
             })
 
 JS;
+    }
+    
+    /**
+    * Returns the value property with property name and value followed by a comma.
+    *
+    * @return string
+    */
+    protected function buildJsPropertyValue()
+    {
+        $src = $this->buildJsValue();
+        if ($src === '') {
+            return '';
+        }
+        return <<<JS
+            src: {$src},
+JS;
+    }
+    
+    /**
+     * Returns inline javascript code for the value of the value property (without the property name).
+     *
+     * Possible results are a quoted JS string, a binding expression or a binding object.
+     *
+     * @return string
+     */
+    public function buildJsValue()
+    {
+        if ($staticIcon = $this->getWidget()->getIcon()) {
+            return '"' . $this->getIconSrc($staticIcon) . '"';
+        }
+        return parent::buildJsValue();
+    }
+   
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\UI5Facade\Facades\Elements\UI5Value::buildJsLabelWrapper()
+     */
+    protected function buildJsLabelWrapper($element_constructor)
+    {
+        if ($this->getCaption() === '') {
+            $widget = $this->getWidget();
+            if ($widget->hasValueWidget() === true) {
+                $widget->setCaption($widget->getValueWidget()->getCaption());
+            } else {
+                return $element_constructor;
+            }
+        }
+        return parent::buildJsLabelWrapper($element_constructor);
     }
 }

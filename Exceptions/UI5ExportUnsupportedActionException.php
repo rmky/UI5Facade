b8ac67060db65888a0d606da037964a3ec2f7ab9
Ralf Mulansky <ralf.mulansky@salt-solutions.de>
2019-09-17 16:59:24 +0200

    NEW Exception for UnsupportedAction when exporting Fiori Apps

---
+++ Exceptions/UI5ExportUnsupportedActionException.php
@@ -0,0 +1,15 @@
+<?php
+namespace exface\UI5Facade\Exceptions;
+
+use exface\Core\Exceptions\InvalidArgumentException;
+
+/**
+ * Exception thrown if the Fiori exporter cannot deal with an action.
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
+class UI5ExportUnsupportedActionException extends InvalidArgumentException
+{
+    
+}

7a62422ed3850965b60395eedaacd89a9dcdb136
Andrej Kabachnik <andrej_ka@gmx.de>
2019-12-02 23:21:03 +0100

    FIX various problems with sap.m.StepInput by using StepInputCustom

--- Facades/Elements/UI5InputNumber.php
+++ Facades/Elements/UI5InputNumber.php
@@ -2,11 +2,15 @@
 namespace exface\UI5Facade\Facades\Elements;
 
 use exface\Core\DataTypes\NumberDataType;
-use exface\Core\Widgets\InputNumber;
 
 /**
- * Renders a numeric input widget as sap.m.StepInput for regular numbers or sap.m.Input
- * for hexadecimal numbers or numeric ids.
+ * Renders a numeric input widget as a custom version of sap.m.StepInput for regular 
+ * numbers or sap.m.Input for hexadecimal numbers or numeric ids.
+ * 
+ * A custom version of sap.m.StepInput is used because the original does not allow
+ * empty values, which is unexceptable in various situation like in filters (no
+ * way to deaktivate the filter) or optional inputs (empty inputs are treated as 
+ * zero!).
  *
  * @method InputNumber getWidget()
  * 
@@ -15,73 +19,22 @@ use exface\Core\Widgets\InputNumber;
  */
 class UI5InputNumber extends UI5Input
 {
-    protected function init()
-    {
-        parent::init();
-        
-        // The sap.m.StepInput cannot be empty for some reason, so we need this workaround
-        // script to empty the value when it is not set or explicitly removed by the user.
-        // The script keeps a "raw value" as a data-attribute of the control and updates it
-        // with every keyup event. Whenever UI5 would check for a change (that is on blur or enter),
-        // the script checks if the raw value is empty and empties the control - regardless
-        // of whether UI5 thinks it's a change or not (the UI5 change event was not enough
-        // because it only fires when the value actually did change). 
-        if ($this->isStepInput()) {
-            $onAfterRendering = <<<JS
-
-                    var oStepInput =  oEvent.srcControl;
-                    var val = oStepInput.data('_rawValue');
-                    var eInput = $('#{$this->getId()}-input-inner');
-                    eInput.val({$this->buildJsInitialValue()});
-                    eInput
-                        .keyup(function(event){
-                            oStepInput.data('_rawValue', eInput.val());
-                        })
-                        .blur(function(event){
-                            if (oStepInput.data('_rawValue') === ''){
-                                setTimeout(function(){eInput.val('')}, 5);
-                                event.stopPropagation();
-                                event.preventDefault();
-                                return false;
-                            }
-                        })
-                        .keypress(function(e) {
-                            var keycode = (e.keyCode ? e.keyCode : e.which);
-                            if (keycode == '13') {
-                                if (oStepInput.data('_rawValue') === ''){
-                                    setTimeout(function(){eInput.val('')}, 5);
-                                    event.stopPropagation();
-                                    event.preventDefault();
-                                    return false;
-                                }
-                            }
-                        });
-                    oStepInput.data('_rawValue', {$this->buildJsInitialValue()});
-
-JS;
-            $this->addPseudoEventHandler('onAfterRendering', $onAfterRendering);
-            
-            // On the other hand, once UI5 does fire a change, the raw value is updated to 
-            // make sure the + and - buttons do not loose their functionality.
-            $onChange = <<<JS
-
-                    if (oEvent.getSource && (oEvent.getSource().data('_rawValue') !== '' || oEvent.getParameters().value !== 0)) {
-                        oEvent.getSource().data('_rawValue', oEvent.getParameters().value);
-                    }
-
-JS;
-            $this->addOnChangeScript($onChange);
-        }
-    }
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\UI5Facade\Facades\Elements\UI5Input::buildJsConstructorForMainControl()
+     */
     public function buildJsConstructorForMainControl($oControllerJs = 'oController')
     {
         if (! $this->isStepInput()) {
             return parent::buildJsConstructorForMainControl();
         }
         
+        $this->getController()->addExternalModule('libs.exface.ui5Custom.StepInputCustom', 'exface/vendor/exface/UI5Facade/Facades/js/ui5Custom/StepInputCustom');
+        
         return <<<JS
 
-            new sap.m.StepInput("{$this->getId()}", {
+            new exface.ui5Custom.StepInputCustom("{$this->getId()}", {
                 {$this->buildJsProperties()}
                 {$this->buildJsPropertiesMinMax()}
                 {$this->buildJsPropertyStep()}
@@ -95,7 +48,7 @@ JS;
     /**
      * @return string
      */
-    protected function buildJsPropertyStep()
+    protected function buildJsPropertyStep() : string
     {
         $widget = $this->getWidget();
         if (! is_null($widget->getStep())) {
@@ -107,7 +60,11 @@ JS;
         return 'step: ' . $value . ',';
     }
     
-    protected function buildJsPropertyPrecision()
+    /**
+     * 
+     * @return string
+     */
+    protected function buildJsPropertyPrecision() : string
     {
         if (is_null($val = $this->getWidget()->getPrecisionMax())) {
             $val = 2;
@@ -116,7 +73,7 @@ JS;
         return 'displayValuePrecision: ' . $val . ',';
     }
     
-    protected function buildJsPropertiesMinMax()
+    protected function buildJsPropertiesMinMax() : string
     {
         $widget = $this->getWidget();
         $options = '';
@@ -142,25 +99,6 @@ JS;
         
         return 'type: sap.m.InputType.Number,';
     }
-    
-    public function buildJsValueGetter(){
-        if (! $this->isStepInput() || $this->getWidget()->isRequired()) {
-            return parent::buildJsValueGetter();
-        }
-        return <<<JS
-
-function(){
-    var oStepInput = sap.ui.getCore().byId('{$this->getId()}');
-    if (oStepInput.data('_rawValue') === '') {
-        return '';
-    } else if (oStepInput.data('_rawValue') === null) {
-        return {$this->buildJsInitialValue()};
-    }
-    return oStepInput.getValue();
-}()
-
-JS;
-    }
         
     /**
      * Returns the initial value defined in UXON as number or an quoted empty string
@@ -168,13 +106,17 @@ JS;
      * 
      * @return string|NULL
      */
-    protected function buildJsInitialValue()
+    protected function buildJsInitialValue() : string
     {
         $val = $this->getWidget()->getValueWithDefaults();
         return (is_null($val) || $val === '') ? '""' : $val;
     }
         
-    protected function isStepInput()
+    /**
+     * 
+     * @return boolean
+     */
+    protected function isStepInput() : bool
     {
         $dataType = $this->getWidget()->getValueDataType();
         return (! ($dataType instanceof NumberDataType) || ($dataType->getBase() === 10 && ! $dataType->is('exface.Core.NumericId')));

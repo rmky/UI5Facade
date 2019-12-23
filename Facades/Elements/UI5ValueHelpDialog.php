31787fee4cb94b3f1fca78b374b7ba76b8631f0f
aka <andrej.kabachnik@salt-solutions.de>
2019-12-23 16:25:50 +0100

    REFACTOR added UI5ValueHelpDialog element

---
+++ Facades/Elements/UI5ValueHelpDialog.php
@@ -0,0 +1,68 @@
+<?php
+namespace exface\UI5Facade\Facades\Elements;
+
+/**
+ * Generates a sap.m.Dialog for a Value-Help-Dialog.
+ * 
+ * ## How to use
+ * 
+ * Instantiate the dialog in the handler script of the button-press-event
+ * for the button, that should open the dialog.
+ * 
+ * ```php
+ * 
+ * $vhpd = new UI5ValueHelpDialog($tableWidgetToRenderInTheDialog, $facade);
+ * $vhpd->addOnCloseScript('alert("You have selected: "' . $vhpd->buildJsValueGetter() . ')');
+ * $vhpdConstrJs = $vhpd->buildJsConstructor($oControllerJs);
+ * $resultingJS = <<<JS
+ *      
+ *      var oDialog = {$vhpdConstrJs};
+ *      oDialog.open();
+ *  
+ * JS;
+ * 
+ * ```
+ *
+ * @author Andrej Kabachnik
+ *        
+ */
+class UI5ValueHelpDialog extends UI5DataTable
+{
+    public function buildJsConstructor($oControllerJs = 'oController') : string
+    {
+        $this->getPaginatorElement()->registerControllerMethods();
+        return $this->buildJsValueHelpDialogConstructor($oControllerJs);
+    }   
+                    
+    protected function buildJsValueHelpDialogConstructor(string $oControllerJs) : string
+    {
+        return <<<JS
+
+            new sap.m.Dialog({
+                content: [
+                    todo: "Title, etc.",
+                    {$this->buildJsValueHelpTableWrapper($oControllerJs, $this->buildJsConstructorForControl($oControllerJs))},
+                ],
+                todo: "toolbar",
+                close: {$this->getController()->buildJsEventHandler($this, 'close', true)}
+            })
+
+JS;
+    }
+    
+    protected function buildJsValueHelpTableWrapper(string $oControllerJs, string $tableConstructorJs) : string
+    {
+        // TODO add a special table wrapper instead of UI5DataElementTrait::buildJsPage().
+        return $tableConstructorJs;
+    }
+    
+    /**
+     * 
+     * @param string $js
+     * @return UI5ValueHelpDialog
+     */
+    public function addOnCloseScript(string $js) : UI5ValueHelpDialog
+    {
+        $this->getController()->addOnEventScript($this, 'close', $js);
+    }
+}

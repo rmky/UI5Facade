54c874c4e9a7772dd6aefca80165f8e2a2345819
tmc <tmc@NBDR211.salt-solutions.de>
2020-01-13 10:18:04 +0100

    DOC added some documentation

--- Facades/Elements/UI5Dashboard.php
+++ Facades/Elements/UI5Dashboard.php
@@ -4,6 +4,17 @@ namespace exface\UI5FAcade\Facades\Elements;
 use exface\Core\Widgets\Dashboard;
 
 /**
+ * A `Dashboard` is a Widget to display multiple Widgets in an grid-like layout, to let the user have an
+ * easy and instant overview about certain data.
+ * This `Dashboard` in UI5 consists of `sap.f.GridContainer`'s, which align the child widgets 
+ * in a grid. There might be used multiple instances of `sap.f.Gridcontainer` for one `Dashboard` to support
+ * not only the use of units like '%' or 'px' as width parameters, but also the use of an `integer` to define the
+ * number of columns one Widget uses.
+ * The children Widgets itself are wrapped in instances of `sap.f.Card`.
+ * The `GridContainer` always has the parameters `allowDenseFill` and `snapToRow` set to `true`
+ * to automatically optimize the alignment of the Cards. Furthermore the `GridContainer` may use 
+ * the css-class `dashboard_gridcontainer_layout`, which sets the minimum and maximum width
+ * of its child elements, if the size of those elements is only given by column-count.
  * 
  * @author tmc
  *
@@ -20,21 +31,13 @@ class UI5Dashboard extends UI5Panel
      */
     public function buildJsConstructor($oControllerJs = 'oController') : string
     {   
-        $dashboard = $this->buildJsConstructorForDashboard();
-        
-        return $dashboard;
+        return $this->buildJsConstructorForDashboard();
     }
     
     /**
      * This function returns the JS-code for the `Dashboard`. 
-     * A `Dashboard` in UI5 consists of a `sap.f.GridContainer`, which aligns the child widgets 
-     * in a grid. 
-     * The children Widgets itself are wrapped in instances of `sap.f.Card`, whose code is getting
-     * created by call of the function `buildDashboardContentWrapper()`.
-     * The `GridContainer` always has the parameters `allowDenseFill` and `snapToRow` set to `true`
-     * to automatically optimize the alignment of the Cards. Furthermore the `GridContainer` uses 
-     * the css-class `dashboard_gridcontainer_layout`, which sets the minimum and maximum width
-     * of its child elements. 
+     * A Dashboard is getting wrapped in a `sap.m.ScrollContainer` to allow scrolling through the elements of
+     * a dashboard.
      * 
      * @return string
      */
@@ -62,7 +65,19 @@ JS;
      * in the UXON of the `Dashbord`. For each widget in the `Dashboard`, a `Card` gets created. 
      * The `Card` gets its layoutdata (e.g. width and height) assigned from the data given in the   
      * widgets UXON and the widget itself is then being inserted in the `Card`'s content.
+     * The parameter of this function is the number of the Gridcontainer for which the cards shall be generated.
      * 
+     * Generating the correct width values for the cards is a bit tricky, because this process needs to be adapted
+     * to the unit the width of the element is given. 
+     *     
+     *      If the width is given as an integer, this number is set directly into the card's 
+     *      `GridContainerItemLayoutData`'s `columns` property. The width of the widget in the card itself
+     *      is set to 100%.
+     * 
+     *      If the width is given as an percentage, the `columns` of the `GridContainerItemLayoutData` are getting
+     *      calulated with the function `getChildrenElementWidthUnitCount()`. The width of the widget in the card
+     *      is then set to 100%.
+     *      
      * @return string
      */
     protected function buildDashboardContentWrapper(int $no) : string
@@ -182,7 +197,8 @@ JS;
         
         foreach ($childWidgets as $widget){
             
-            $widthIsInUnits = is_numeric($widget->getWidth()->getValue());
+            $widthDim = $widget->getWidth();
+            $widthIsInUnits = $widthDim->isRelative() || $widthDim->isUndefined();
             
             if ($modeOfCurrentContainerIsInUnits !== $widthIsInUnits
                 && $modeOfCurrentContainerIsInUnits !== null){

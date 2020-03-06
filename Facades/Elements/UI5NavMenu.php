eb1166af62163ae21744a2398c5f85197afb734a
rml <rml@NBDR326.salt-solutions.de>
2020-03-06 16:59:48 +0100

    FIX missing ; added in UI5NavMenu.php

--- Facades/Elements/UI5NavMenu.php
+++ Facades/Elements/UI5NavMenu.php
@@ -26,7 +26,7 @@ class UI5NavMenu extends UI5AbstractElement
 
 new sap.tnt.NavigationList("{$this->getId()}", {
     items: [{$this->buildNavigationListItems($menu)}]
-})
+});
 
 JS;
         
@@ -48,28 +48,28 @@ JS;
                 $icon = "open-folder";
                 $output .= <<<JS
             
-new sap.tnt.NavigationListItem({
-    icon: "{$icon}",
-    text: "{$node->getName()}",
-    items: [
-        // BOF {$node->getName()} SubMenu
-        
-        {$this->buildNavigationListItems($node->getChildNodes())}
-        
-        // EOF {$node->getName()} SubMenu
-        ],
-    select: function(){sap.ui.core.BusyIndicator.show(0); window.location.href = '{$url}';}
-}),
+        new sap.tnt.NavigationListItem({
+            icon: "{$icon}",
+            text: "{$node->getName()}",
+            items: [
+                // BOF {$node->getName()} SubMenu
+                
+                {$this->buildNavigationListItems($node->getChildNodes())}
+                
+                // EOF {$node->getName()} SubMenu
+                ],
+            select: function(){sap.ui.core.BusyIndicator.show(0); window.location.href = '{$url}';}
+        }),
 
 JS;
             } else {
                 $output .= <<<JS
 
-new sap.tnt.NavigationListItem({
-    icon: "{$icon}", 
-    text: "{$node->getName()}", 
-    select: function(){sap.ui.core.BusyIndicator.show(0); window.location.href = '{$url}';} 
-}),
+        new sap.tnt.NavigationListItem({
+            icon: "{$icon}", 
+            text: "{$node->getName()}", 
+            select: function(){sap.ui.core.BusyIndicator.show(0); window.location.href = '{$url}';} 
+        }),
 
 JS;
             }

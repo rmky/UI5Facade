988d3f07da029e22731fd6ed989f06535e9eb685
rml <rml@NBDR326.salt-solutions.de>
2020-09-15 16:33:44 +0200

    REFACTOR moved facade independent fuctions to library exf_preloader.js

--- Facades/js/openui5.facade.js
+++ Facades/js/openui5.facade.js
@@ -1,8 +1,7 @@
 // Toggle online/offlie icon
 window.addEventListener('online', function(){
 	exfLauncher.toggleOnlineIndicator();
-	exfLauncher.showMessageToast("Just went online! Syncing offline actions");
-	exfPreloader.getActionQueueData('offline')
+	/*exfPreloader.getActionQueueData('offline')
 	.then(function(data) {
 		var count = data.length;
 		if (count > 0){
@@ -14,10 +13,10 @@ window.addEventListener('online', function(){
 			});
 			exfPreloader.syncActionAll(ids)
 			.then(function(){
-				exfPreloader.updateQueueCount()
+				exfPreloaderUI5.updateQueueCount()
 				.then(function(){
 					shell.setBusy(false);
-					exfLauncher.showMessageToast("Actions synced!");
+					exfLauncher.showMessageToast("Just went online! Actions synced!");
 				})
 			})
 		}
@@ -26,7 +25,7 @@ window.addEventListener('online', function(){
 		shell.setBusy(false);
 		console.log(error);
 		exfLauncher.showMessageToast("Syncing actions failed!");
-	})
+	})*/
 });
 window.addEventListener('offline', function(){
 	exfLauncher.toggleOnlineIndicator();
@@ -86,7 +85,7 @@ const exfLauncher = {};
 		                new sap.m.ToolbarSpacer(),
 		                new sap.m.Button("exf-network-indicator", {
 		                    icon: function(){return navigator.onLine ? "sap-icon://connected" : "sap-icon://disconnected"}(),
-		                    text: "0/0",
+		                    text: "{/_network/queueCnt}/{/_network/syncErrorCnt}",
 		                    layoutData: new sap.m.OverflowToolbarLayoutData({priority: "NeverOverflow"}),
 		                    press: function(oEvent){
 								var oButton = oEvent.getSource();
@@ -143,7 +142,7 @@ const exfLauncher = {};
 																					})																					
 																					exfPreloader.syncActionAll(selectedIds)
 																					.then(function(){
-																						exfPreloader.updateQueueCount()
+																						exfPreloaderUI5.updateQueueCount()
 																						.then(function(){
 																							oButton.setBusy(false);
 																							exfLauncher.showMessageToast("Actions synced!");
@@ -159,7 +158,7 @@ const exfLauncher = {};
 																						return;
 																					})
 																					.catch(function(){
-																						exfPreloader.updateQueueCount()
+																						exfPreloaderUI5.updateQueueCount()
 																						.then(function(){
 																							oButton.setBusy(false);
 																							_oLauncher.contextBar.getComponent().showDialog('Offline Queue', 'Error syncing actions!', 'Error');
@@ -195,7 +194,7 @@ const exfLauncher = {};
 																					})
 																					exfPreloader.deleteActionAll(selectedIds)
 																					.then(function(){
-																						exfPreloader.updateQueueCount()
+																						exfPreloaderUI5.updateQueueCount()
 																						.then(function(){
 																							oButton.setBusy(false);
 																							exfLauncher.showMessageToast("Actions deleted!");
@@ -433,7 +432,7 @@ const exfLauncher = {};
 														icon: "sap-icon://unwired",
 														type: "Active",
 														press: function(oEvent){
-															exfPreloader.showStorage();
+															exfPreloaderUI5.showStorage();
 														},
 													}),
 													new sap.m.StandardListItem({
@@ -483,7 +482,7 @@ const exfLauncher = {};
 			}
 		}));
 		
-		exfPreloader.updateQueueCount();
+		exfPreloaderUI5.updateQueueCount();
 		
 		return _oShell;
 	};
@@ -678,168 +677,8 @@ const exfLauncher = {};
 	};
 }).apply(exfLauncher);
 
-const exfPreloader = {};
-(function(){
-	
-	var _preloader = this;
-		
-	var _db = function() {
-		var dexie = new Dexie('exf-preload');
-		dexie.version(1).stores({
-			'preloads': 'id, object',
-			'actionQueue': 'id, object, action'
-		});
-		dexie.open();
-		return dexie;
-	}();
-	
-	var _preloadTable = _db.table('preloads');
-	var _actionsTable = _db.table('actionQueue');
-	
-	this.addPreload = function(sAlias, aDataCols, aImageCols, sPageAlias, sWidgetId){		
-		_preloadTable
-		.get(sAlias)
-		.then(item => {
-			var data = {
-				id: sAlias,
-				object: sAlias
-			};
-			
-			if (aDataCols) { data.dataCols = aDataCols; }
-			if (aImageCols) { data.imageCols = aImageCols; }
-			if (sPageAlias) { data.page = sPageAlias; }
-			if (sWidgetId) { data.widget = sWidgetId; }
-			
-			if (item === undefined) {
-				_preloadTable.put(data);
-			} else {
-				_preloadTable.update(sAlias, data);
-			}
-		})
-		return _preloader;
-	};
-	
-	this.getPreload = function(sAlias, sPageAlias, sWidgetId) {
-		return _preloadTable.get(sAlias);
-	};
-	
-	this.syncAll = function(fnCallback) {
-		var deferreds = [];
-		return _preloadTable.toArray()
-		.then(data => {
-			$.each(data, function(idx, item){
-				deferreds.push(
-			    	_preloader
-			    	.sync(item.object, item.page, item.widget, item.imageCols)
-			    );
-			});
-			// Can't pass a literal array, so use apply.
-			return $.when.apply($, deferreds)
-		})
-		.catch(error => {
-			exfLauncher.contextBar.getComponent().showErrorDialog('See console for details.', 'Preload sync failed!');
-		});
-	};
-	
-	/**
-	 * @return jqXHR
-	 */
-	this.sync = function(sObjectAlias, sPageAlias, sWidgetId, aImageCols) {
-		console.log('Syncing preload for object "' + sObjectAlias + '", widget "' + sWidgetId + '" on page "' + sPageAlias + '"');
-		if (! sPageAlias || ! sWidgetId) {
-			throw {"message": "Cannot sync preload for object " + sObjectAlias + ": incomplete preload configuration!"};
-		}
-		return $.ajax({
-			type: 'POST',
-			url: 'api/ui5',
-			dataType: 'json',
-			data: {
-				action: 'exface.Core.ReadPreload',
-				resource: sPageAlias,
-				element: sWidgetId
-			}
-		})
-		.then(
-			function(data, textStatus, jqXHR) {
-				var promises = [];
-				promises.push(
-					_preloadTable.update(sObjectAlias, {
-						response: data,
-						lastSync: (+ new Date())
-					})
-				);
-				if (aImageCols && aImageCols.length > 0) {
-					for (i in aImageCols) {
-						var urls = data.rows.map(function(value,index) { return value[aImageCols[i]]; });
-						promises.push(_preloader.syncImages(urls));
-					}
-				}
-				return Promise.all(promises);
-			},
-			function(jqXHR, textStatus, errorThrown){
-				exfLauncher.contextBar.getComponent().showAjaxErrorDialog(jqXHR);
-				return textStatus;
-			}
-		);
-	};
-	
-	this.syncImages = function (aUrls, sCacheName = 'image-cache') {
-		if (window.caches === undefined) {
-			console.error('Cannot preload images: Cache API not supported by browser!');
-			return;
-		}
-		
-		return window.caches
-		.open(sCacheName)
-		.then(cache => {
-			// Remove duplicates
-			aUrls = aUrls.filter((value, index, self) => { 
-			    return self.indexOf(value) === index;
-			});
-			// Fetch and cache images
-			var requests = [];
-			for (var i in aUrls) {
-				if (! aUrls[i]) continue;
-				var request = new Request(aUrls[i]);
-				requests.push(
-					fetch(request.clone())
-					.then(response => {
-						// Check if we received a valid response
-						if(! response || response.status !== 200 || response.type !== 'basic') {
-						  return response;
-						}
-						
-						// IMPORTANT: Clone the response. A response is a stream
-						// and because we want the browser to consume the response
-						// as well as the cache consuming the response, we need
-						// to clone it so we have two streams.
-						var responseToCache = response.clone();
-						
-						return cache.put(request, responseToCache);
-					})
-				);
-			}
-			return Promise.all(requests);
-		});
-	};
-	
-	this.reset = function() {
-		var clear = _preloadTable.toArray()
-		.then(function(dbContent) {
-			var promises = [];
-			dbContent.forEach(function(element) {
-				promises.push(
-					_preloadTable.update(element.id, {
-						response: {},
-						lastSync: 'not synced'
-					})
-				);
-			});
-			return Promise.all(promises);
-		});
-		return clear;
-	};
-	
+const exfPreloaderUI5 = {};
+(function(){	
 	this.showStorage = async function() {
 		console.log('Storage clicked');
 		//check if service worker supported
@@ -908,7 +747,7 @@ const exfPreloader = {};
 				upperCase: false
 			}))
 		}
-		promise = _preloadTable.toArray()
+		promise = exfPreloader.getPreloadTable().toArray()
 		.then(function(dbContent){
 			list.addItem(new sap.m.GroupHeaderListItem({
 				title: 'Synced content',
@@ -978,116 +817,6 @@ const exfPreloader = {};
 		return;
 	};
 	
-	this.addAction = function(offlineAction, objectAlias) {
-		var date = (+ new Date());
-		var data = {
-			id: date,
-			object: objectAlias,
-			action: offlineAction.data.action,
-			request: offlineAction,
-			triggered: new Date(date).toLocaleString(),
-			status: 'offline',
-			tries: 0
-		};
-		if (offlineAction.headers) {
-			data.headers = offlineAction.headers
-		}
-		return _actionsTable.put(data)
-		.then(function(){
-			return exfPreloader.updateQueueCount();
-		});
-	};
-	
-	this.getActionQueueData = function(filter) {
-		return _actionsTable.toArray()
-		.then(function(dbContent) {
-			var oData = {}
-			var data = [];
-			dbContent.forEach(function(element) {
-				if (element.status != filter) {
-					return;
-				}
-				item = {
-						id: element.id,
-						action_alias: element.action,
-						object: element.object,
-						triggered: element.triggered,
-						status: element.status,
-						tries: element.tries
-				}
-				if (element.response) {
-					item.response = element.response;
-				}
-				data.push(item);
-				return;
-			})
-			data = data;
-			return data;
-		})
-		.catch(function(error) {
-			return [];
-		})
-	};
-	
-	this.syncActionAll = function(selectedIds) {
-		var promises = [];
-		selectedIds.forEach(function(id){
-			promises.push(_preloader.syncAction(id));
-		});
-		return Promise.all(promises);
-	};
-	
-	this.syncAction = function(id) {
-		return _actionsTable.get(id)
-		.then(function(element){
-			var ajaxObject = element.request;
-			ajaxObject.timeout = 5000;
-			ajaxObject.success = function(data, textStatus, jqXHR) {
-				console.log('Success sending');
-				return _actionsTable.update(element.id, {
-					status: 'success',
-					tries: element.tries + 1,
-					response: data
-				})
-				.then(function (updated){					
-					if (updated) {
-						console.log ("Action was updated");
-					} else {
-						console.log ("Nothing was updated - there was no action with id: ", element.id);
-					}
-				});
-			}
-			ajaxObject.error = function(jqXHR, textStatus, errorThrown) {
-				if (textStatus === 'timeout' || jqXHR.status === 0 || jqXHR.status >= 500) {
-					console.log('Timeout sync action');
-					return _actionsTable.update(element.id, {
-						tries: element.tries + 1,
-						response: textStatus
-					});
-				}
-				console.log('Error Server response');
-				return _actionsTable.update(element.id, {
-					status: 'error',
-					tries: element.tries + 1,
-					response: jqXHR.responseText
-				})
-			}
-			return $.ajax(ajaxObject);
-		});
-	};
-	
-	this.deleteActionAll = function(selectedIds) {
-		var promises = [];
-		selectedIds.forEach(function(id){
-			promises.push(_preloader.deleteAction(id));
-		});
-		return Promise.all(promises);
-	};
-	
-	this.deleteAction = function(id) {
-		return _actionsTable.delete(id)
-	}
-	
 	this.updateQueueCount = function() {
 		return exfPreloader.getActionQueueData('offline')
 		.then(function(data) {
@@ -1096,8 +825,4 @@ const exfPreloader = {};
 			console.log('Queue count updated');
 		})
 	}
-	
-	this.getActionsTable = function() {
-		return _actionsTable;
-	}
-}).apply(exfPreloader);
+}).apply(exfPreloaderUI5);

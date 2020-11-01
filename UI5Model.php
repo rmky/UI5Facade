8ea99ff8e5051be1e514d68f31841c54df2271a7
aka <aka@NBDR223.salt-solutions.de>
2020-11-01 14:44:20 +0100

    FIX improved binding conflict detection in UI5 model

--- UI5Model.php
+++ UI5Model.php
@@ -4,6 +4,8 @@ namespace exface\UI5Facade;
 use exface\Core\Interfaces\WidgetInterface;
 use exface\Core\Interfaces\DataSheets\DataPointerInterface;
 use exface\UI5Facade\Facades\Interfaces\UI5ModelInterface;
+use exface\Core\Interfaces\Widgets\iShowSingleAttribute;
+use exface\Core\Interfaces\Widgets\iShowDataColumn;
 
 class UI5Model implements UI5ModelInterface
 {    
@@ -98,10 +100,33 @@ class UI5Model implements UI5ModelInterface
      */
     public function hasBindingConflict(WidgetInterface $widget, string $bindingName) : bool
     {
+        // If there is not binding for the given widget and property, check if they conflict
+        // with another binding
         if (! $this->hasBinding($widget, $bindingName)) {
+            // Iterat through bindings looking for those with the same binding name, but a different
+            // widget id.
             foreach ($this->bindings as $widgetId => $bindings) {
                 if ($bindings[$bindingName] !== null && $widgetId !== $widget->getId()) {
-                    return true;
+                    $bindingWidget = $widget->getPage()->getWidget($widgetId);
+                    // If the other binding's widget or the current widget are not showing a single attribute,
+                    // it's really strange - so treat that as a potential conflict :)
+                    if (! ($bindingWidget instanceof iShowSingleAttribute) || ! ($widget instanceof iShowSingleAttribute)) {
+                        return true;
+                    }
+                    // Same goes for the case, that one of the widgets is not bound to a data column
+                    if (! ($bindingWidget instanceof iShowDataColumn) || ! ($widget instanceof iShowDataColumn)) {
+                        return true;
+                    }
+                    
+                    // If both can be bound to an attribute, but at least one is not, AND both have
+                    // the same data column name - it's OK, the same value is meant.
+                    if ((! $bindingWidget->isBoundToAttribute() || ! $widget->isBoundToAttribute()) && $bindingWidget->getDataColumnName() === $widget->getDataColumnName()) {
+                        continue;
+                    }
+                    // If both are bound to an attribute, but the attributes are different, it's a conflict!
+                    if (! $bindingWidget->getAttribute()->is($widget->getAttribute())) {
+                        return true;
+                    }
                 }
             }
         }

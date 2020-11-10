3fb12e405a124fd8596aca82f1899f132dc9bd60
aka <aka@NBDR223.salt-solutions.de>
2020-11-10 15:36:18 +0100

    FIX SQL error in migration

--- Install/Sql/MySQL/Migrations/1.0/20201109_2114_01_NEW_more_export_options.sql
+++ Install/Sql/MySQL/Migrations/1.0/20201109_2114_01_NEW_more_export_options.sql
@@ -4,7 +4,7 @@ ALTER TABLE `fiori_webapp`
 	ADD COLUMN `odata_export_credentials` TINYINT(1) NOT NULL DEFAULT '0' AFTER `odata_use_relative_urls`,
 	ADD COLUMN `odata_export_sap_client` TINYINT(1) NOT NULL DEFAULT '0' AFTER `odata_export_credentials`;
 	
-ALTER TABLE `fiori_webapp` SET `odata_export_sap_client` = 1;
+UPDATE `fiori_webapp` SET `odata_export_sap_client` = 1;
 	
 -- DOWN
 
